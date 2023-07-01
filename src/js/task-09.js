1ba74406632001dccf88e2562d67d35914b5cb3f
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-01 19:31:48 +0300

    hw06js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,5 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const textColor = document.querySelector('.color');
+const changeColorBtn = document.querySelector('.change-color');
+const body = document.body;
+
+const changeColor = () => {
+  textColor.textContent = body.style.backgroundColor = getRandomHexColor();
+};
+// const changeColor = () => {
+//   textColor.textContent = getRandomHexColor();
+//   body.style.backgroundColor = textColor.textContent;
+// };
+
+changeColorBtn.addEventListener('click', changeColor);
--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+createBtnEl.addEventListener('click', () => {
+  //console.log(inputNumberEl.value);
+  if (
+    Number(inputNumberEl.value.trim()) > Number(inputNumberEl.max) ||
+    Number(inputNumberEl.value.trim()) < Number(inputNumberEl.min)
+  ) {
+    alert('Please enter number from 1 to 100');
+  } else {
+    createBoxes(inputNumberEl.value.trim());
+  }
+  inputNumberEl.value = '';
+});
+
+destroyBtnEl.addEventListener('click', destroyBoxes);
+
+function destroyBoxes() {
+  inputNumberEl.value = '';
+  divBoxesEl.innerHTML = '';
 }
