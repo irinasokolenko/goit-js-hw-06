3de27782744a028b73742f0f520e927f1dc3d38c
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-07-02 17:26:25 +0300

    hw06js

--- src/js/task-01.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const item = document.querySelectorAll('.item');
-Array.prototype.forEach.call(item, (element) => {
-    const title = element.querySelector('h2').innerHTML;
-    const itemsLength = element.querySelectorAll('li').length;
-    console.dir(`Категорія: ${title} / Кількість елементів: ${itemsLength}`);
-});
